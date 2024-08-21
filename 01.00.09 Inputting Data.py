da1f2e0636ec6b281bb9060e56250ac7b4ba3d4a
JesseTaylor4 <jtaylor1@ualr.edu>
2024-08-21 15:32:04 +0000

    first day

---
+++ 01.00.09 Inputting Data.py
@@ -0,0 +1,5 @@
+firstname = input("Enter your name:")  # Input statement without a prompt
+print("You Entered: " + firstname)
+ 
+lastname = input("Enter Your Name:")  # Input statement with a prompt
+print("You Entered: " + lastname)
